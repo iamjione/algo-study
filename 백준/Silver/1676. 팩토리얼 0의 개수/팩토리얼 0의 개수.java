8666050376ea54883c921ab3c99d5d86baac767e
지온 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2023-03-03 14:05:30 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 108 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int cnt = 0 ;
+		
+		while(N>=5) {
+			cnt += N/5;
+			N /= 5;
+		}
+
+		System.out.println(cnt);
+		
+	}
+}

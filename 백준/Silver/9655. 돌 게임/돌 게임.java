23ea0f5dba658b2f528b4453653424ea2f8dc8f2
지온 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2023-03-05 00:54:21 +0900

    Update 돌 게임.java

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -5,6 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();//돌 개수
+		
+		//돌 1개 >> 상근 승
+		//돌 2개 >> 상근 1개 창영 1개 창영 승
+		//돌 3개 >> 상근 3개 상근 승
+		//돌 4개 >> 상근 3개 창영 1개 / 상근 1개 창영 3개 창영 승
+		//돌 개수 짝수일 때 창영이가 이기고 홀수면 상근이가 이김
+		
 		System.out.println((N%2==0)?"CY":"SK");
 	}
 }

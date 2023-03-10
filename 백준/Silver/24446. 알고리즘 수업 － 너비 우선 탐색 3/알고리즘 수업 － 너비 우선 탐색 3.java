6d1361250f77d9513863e223d391d33a4e0e593b
지온 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2023-03-10 17:38:07 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 3, Time: 824 ms, Memory: 89200 KB -BaekjoonHub

---
+++ 백준/Silver/24446. 알고리즘 수업 － 너비 우선 탐색 3/알고리즘 수업 － 너비 우선 탐색 3.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M;
+	static ArrayList<Integer>[] adjList;
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());//정점 개수
+		M = Integer.parseInt(st.nextToken());//간선 개수
+		int R = Integer.parseInt(st.nextToken());//시작 정점
+		
+		arr = new int[N+1];//답으로 출력할 정점 깊이 저장할 배열
+		for(int i=0; i<N+1; i++) arr[i] = -1;//초기값 -1로!(방문되지 않은 노드의 깊이 == -1)
+		
+		adjList = new ArrayList[N+1];//head가 모두 null인 상태
+		
+		for(int i=1; i<N+1; i++) {//비어있는 ArrayList 만들어주기
+			adjList[i] = new ArrayList<Integer>();
+		}
+		
+		int u,v;//간선 정보
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			u = Integer.parseInt(st.nextToken());
+			v = Integer.parseInt(st.nextToken());
+			//무방향 그래프니까 양방향 모두 저장
+			adjList[u].add(v);
+			adjList[v].add(u);
+		}
+		
+		bfs(R);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<N+1; i++) sb.append(arr[i]).append('\n');
+		System.out.println(sb);
+
+	}
+	
+	//R번 정점부터 너비우선탐색하는 함수
+	private static void bfs(int R) {
+		Queue<Integer> q = new ArrayDeque<>();//탐색 대상 결정하기 위한 큐
+		boolean[] visited = new boolean[N+1];//방문 표시할 boolean배열
+		
+		//시작 정점 큐에 넣어주고 방문 표시 해줌
+		q.offer(R);
+		visited[R] = true;
+		
+		arr[R] = 0;//배열에 시작 정점 깊이 저장
+		
+		int curr = 0;//탐색 정점 번호 0으로 초기화
+		while(!q.isEmpty()) {
+			curr = q.poll();//큐에서 poll한 정점을 현재 탐색 정점으로!
+			
+			for(int vertex : adjList[curr]) {//탐색 정점의 인접 정점에 대해서
+				if(!visited[vertex]) {//아직 방문하지 않은 정점이면
+					q.offer(vertex);//큐에 넣어주고
+					visited[vertex] = true;//방문했다고 표시 해줌
+					arr[vertex] = arr[curr]+1;//나의 정점 깊이 = 나를 불러낸 정점 깊이+1
+				}
+			}
+		}
+	}
+
+}

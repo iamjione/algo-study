ff2eab40596face8251125aa4195e30774f10d90
지온 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2023-03-21 17:06:18 +0900

    [Silver V] Title: 집합, Time: 1088 ms, Memory: 312044 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,44 +1,47 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();//연산 개수
-		Set<Integer> s = new HashSet<>();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st; 
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<N; i++) {
-			String order = sc.next();
-			int num;
-			switch(order){
-			case "add":
-				s.add(sc.nextInt());
-				break;
-			case "remove":
-				num = sc.nextInt();
-				if(s.contains(num)) s.remove(num);
-				break;
-			case "check":
-				if(s.contains(sc.nextInt())) sb.append(1).append('\n');
-				else sb.append(0).append('\n');
-				break;
-			case "toggle":
-				num = sc.nextInt();
-				if(s.contains(num)) s.remove(num);
-				else s.add(num);
-				break;
-			case "all":
-				s.removeAll(s);
-				for(int n=1; n<=20; n++) {
-					s.add(n);
+		
+		int M = Integer.parseInt(br.readLine());
+		int S = 0;
+		int n;
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			String str = st.nextToken();
+			if (str.equals("add")) {
+				n = Integer.parseInt(st.nextToken());
+				S |= 1<<n;
+			} else if(str.equals("check")) {
+				n = Integer.parseInt(st.nextToken());
+				if((S & (1<<n)) > 0) {
+					sb.append(1).append("\n");
+				} else {
+					sb.append(0).append("\n");
 				}
-				break;
-			case "empty":
-				s.removeAll(s);
+			} else if(str.equals("remove")) {
+				n = Integer.parseInt(st.nextToken());
+				S &= ~(1<<n);
+			} else if(str.equals("toggle")) {
+				n = Integer.parseInt(st.nextToken());
+				S ^= (1<<n);
+			} else if(str.equals("all")) {
+				S = (1<<21) -1;
+			} else if(str.equals("empty")) {
+				S = ~((1<<21) -1);
 			}
+			
 		}
 		System.out.println(sb);
 	}
+
 }

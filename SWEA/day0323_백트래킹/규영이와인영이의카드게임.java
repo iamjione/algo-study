700b600ce6de5c1e15d6066c429352de5ba58de8
지온 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2023-03-23 20:38:25 +0900

    Update and rename SWEA/day0323_백트레킹/규영이와인영이의카드게임.java to SWEA/day0323_백트래킹/규영이와인영이의카드게임.java

--- SWEA/day0323_백트래킹/규영이와인영이의카드게임.java
+++ SWEA/day0323_백트래킹/규영이와인영이의카드게임.java
@@ -1,10 +1,10 @@
-package 백트레킹;
+package 백트래킹;
 
 import java.util.Scanner;
 
 public class 규영이와인영이의카드게임 {
 
-	static int[] arr = new int[9];
+	static int[] arr = new int[10];
 	static boolean[] select;
 	static int win, lose;
 	
@@ -14,8 +14,9 @@ public class 규영이와인영이의카드게임 {
 		for(int tc=1; tc<=T; tc++) {
 			select = new boolean[19];
 			
-			//규영이가 받은 카드번호 1로 표시
-			for(int i=0; i<9; i++) {
+			//규영이가 받은 카드번호 arr배열에 순서대로 저장하고
+			//select 배열에 사용했다고 표시해줌
+			for(int i=1; i<10; i++) {
 				arr[i] = sc.nextInt();
 				select[arr[i]] = true;
 			}
@@ -23,33 +24,33 @@ public class 규영이와인영이의카드게임 {
 			win = 0;//규영이가 이긴 횟수
 			lose = 0;//규영이가 진 횟수
 			
-			game(0,0,0);
+			game(1,0,0);
 			
 			System.out.println("#"+tc+" "+win+" "+lose);
 		}
 
 	}
 
-	//idx:게임횟수 0~8까지 9번 
+	//idx:게임횟수 1~9 
 	//A: 규영이 점수 합
 	//B: 인영이 점수 합
 	private static void game(int idx, int A, int B) {
-		if(idx==9) {//게임끝났을 때
+		if(idx==10) {//게임끝났을 때
 			if(A>B) win++; //규영이 점수가 더 높으면 이긴 횟수 증가
 			else if(A<B) lose++;//인영이 점수가 더 높으면 진 횟수 증가
 			return;
 		}
 		
 		for(int i=1; i<19; i++) {
-			//규영이 카드거나 인영이가 이미 쓴 카드면 못 쓰니까 컨티뉴
+			//규영이가 가진 카드거나 인영이가 이미 쓴 카드면 못 쓰니까 컨티뉴
 			if(select[i]) continue;
 			
 			//사용한 카드 체크해줌
 			select[i] = true;
 			//규영이가 낸 카드 숫자가 더 작을 때
-			if(arr[idx]<i) game(idx+1, A, B+i-arr[idx]);
+			if(arr[idx]<i) game(idx+1, A, B+i+arr[idx]);
 			//규영이가 낸 카드 숫자가 더 클 때
-			else game(idx+1, A+arr[idx]-i, B);
+			else game(idx+1, A+arr[idx]+i, B);
 			select[i] = false;//카드 사용 표시 원상복구	
 		}
 		

904b49cd0dbc785341570cd3f4cf708bbdf7af55
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2023-06-22 12:29:23 +0900

    [Gold III] Title: 파티, Time: 380 ms, Memory: 34080 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
 public class Main {
-	
-	static int N, X;
-	static ArrayList<Node>[] adjList;
-	static int[] time;//최소 소요 시간 저장할 배열
-	static boolean[] visited;//방문체크 배열
+	/*
+	 * [다익스트라 2번만 써서 푸는 방법]
+	 * 간선 방향 반대로 입력받아서 X에서 출발하는 다익스트라 실행하면
+	 * 각 마을에서 X마을로 가는 최단 시간 구할 수 있음
+	 */
+	static int N,X;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -13,40 +14,40 @@ public class Main {
 		int M = sc.nextInt();//도로 수
 		X = sc.nextInt();//파티여는 마을 번호
 		
-		adjList = new ArrayList[N+1];
-		for(int i=1; i<N+1; i++) adjList[i] = new ArrayList<>();
+		ArrayList<Node>[] adjList = new ArrayList[N+1];//간선 방향 원래대로 저장할 인접리스트
+		ArrayList<Node>[] reverseAdjList = new ArrayList[N+1];//간선 방향 반대로 저장할 인접 리스트
+		for(int i=1; i<N+1; i++) {
+			adjList[i] = new ArrayList<>();
+			reverseAdjList[i] = new ArrayList<>();
+		}
+		
 		for(int i=0; i<M; i++) {
 			int u = sc.nextInt();//시작점
 			int v = sc.nextInt();//끝점
 			int w = sc.nextInt();//소요 시간
-			//단방향 도로
+
 			adjList[u].add(new Node(v,w));
+			reverseAdjList[v].add(new Node(u,w));
 		}
 		
 		//X마을에서 모든 마을까지의 최소 소요 시간 구하기
-		time = new int[N+1];
-		Arrays.fill(time, Integer.MAX_VALUE);//최대값으로 초기화
-		time[X] = 0;//출발점에서 출발점까지 소요시간==0		
-		visited = new boolean[N+1];
-		dijkstra();
-		int[] minTime = new int[N+1];
-		minTime = time.clone();//X마을에서 모든 마을까지 걸린 최소시간 minTime에 저장
+		int[] time1 = dijkstra(adjList);
 		
 		//모든 마을에서 X마을까지의 최소 소요 시간 구하기
-		for(int i=1; i<N+1; i++) {
-			Arrays.fill(time, Integer.MAX_VALUE);
-			time[i] = 0;
-			visited = new boolean[N+1];
-			dijkstra(i);
-			minTime[i] += time[X];//X마을까지 걸린 시간 minTime에 더하기
-		}
+		int[] time2 = dijkstra(reverseAdjList);
 		
 		int max = 0;//최대 소요시간
-		for(int i=1; i<N+1; i++) max = Math.max(max, minTime[i]);
+		for(int i=1; i<N+1; i++) max = Math.max(max, time1[i]+time2[i]);
 		System.out.println(max);
 	}
 	
-	private static void dijkstra() {
+	private static int[] dijkstra(ArrayList<Node>[] list) {
+		int[] time = new int[N+1];
+		Arrays.fill(time, Integer.MAX_VALUE);//최대값으로 초기화
+		time[X] = 0;//출발점에서 출발점까지 소요시간==0
+		
+		boolean[] visited = new boolean[N+1];
+		
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		pq.add(new Node(X,0));
 		
@@ -56,34 +57,17 @@ public class Main {
 			if(visited[curr]) continue;
 			
 			visited[curr] = true;
-			for(Node n : adjList[curr]) {
+			for(Node n : list[curr]) {
 				int next = n.v;
 				if(visited[next]) continue;
 				time[next] = Math.min(time[next], time[curr]+n.w);
 				pq.add(new Node(next, time[next]));
 			}
 		}
+		
+		return time;
 	}
 
-	private static void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(start,0));
-		
-		while(!pq.isEmpty()) {
-			int curr = pq.poll().v;
-			
-			if(curr==X) return;//X마을에 도착했으면 끝내기
-			if(visited[curr]) continue;
-			
-			visited[curr] = true;
-			for(Node n : adjList[curr]) {
-				int next = n.v;
-				if(visited[next]) continue;
-				time[next] = Math.min(time[next], time[curr]+n.w);
-				pq.add(new Node(next, time[next]));
-			}
-		}
-	}
 
 	private static class Node implements Comparable<Node>{
 		int v,w;

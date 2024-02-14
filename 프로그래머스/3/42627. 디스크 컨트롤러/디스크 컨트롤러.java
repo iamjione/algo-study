fe281c5244d5b0b99bed78fc6f70ad74a8fb4372
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-02-14 19:37:22 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.71 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] jobs) {
+        
+        // 작업을 우선순위 큐에 넣기
+        PriorityQueue<Job> pq = new PriorityQueue<>();
+        
+        for(int i=0; i<jobs.length; i++){
+            pq.add(new Job(jobs[i][0], jobs[i][1]));
+        }
+        
+        // 우선 순위 큐가 빌때까지 현재 실행 가능한 작업 중 가장 빨리 끝나는 작업 실행하기
+        int curr = 0; // 현재 시간
+        int sum = 0; // 모든 작업을 끝내는데 걸린 시간
+        int min = 2000; // 아직 요청이 안 온 작업 중 제일 빠른 요청 시간
+        List<Job> tmp = new LinkedList<>(); // 소요 시간은 짧지만 아직 요청 시간이 안된 작업들의 임시 저장소
+        
+        while(!pq.isEmpty()){
+            Job job = pq.poll();
+            if(job.start <= curr){
+                curr += job.duration;
+                sum += curr - job.start;
+                
+                for(Job j : tmp) pq.add(j);
+                tmp.clear();
+            }else{
+                tmp.add(job);
+                if(min>job.start) min = job.start;
+            }
+            
+            if(pq.isEmpty() && !tmp.isEmpty()){
+                for(Job j : tmp) pq.add(j);
+                tmp.clear();
+                
+                curr = min;
+                min = 2000;
+            }
+        }
+        
+        return sum/jobs.length;
+    }
+    
+    class Job implements Comparable<Job>{
+        int start;
+        int duration;
+        
+        public Job(int start, int duration){
+            this.start = start;
+            this.duration = duration;
+        }
+        
+        // 빨리 끝나는 작업 순으로 정렬
+        @Override
+        public int compareTo(Job o){
+            return this.duration-o.duration;
+        }
+    }
+}

d9338296473eea3125d4400793804cfe5ebd2a4f
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-02-22 23:34:17 +0900

    [level 2] Title: [3차] 압축, Time: 39.99 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -4,30 +4,29 @@ class Solution {
     
     public List<Integer> solution(String msg) {
         List<Integer> answer = new ArrayList<>();
-        List<String> dictionary = new ArrayList<>();
+        Map<String, Integer> dictionary = new HashMap<>();
         boolean[] visited = new boolean[msg.length()];
         
-        // 0번 인덱스 비워두기
-        dictionary.add(null);
-        // 1~26까지의 인덱스에 A~Z 저장
-        for(int i=0; i<26; i++){
-            dictionary.add(String.valueOf((char) (65+i)));
+        // 사전에 A~Z 저장
+        for(int i=1; i<=26; i++){
+            dictionary.put(String.valueOf((char) (64+i)), i);
         }
         
+        int idx = 27; // 사전 색인 번호
         for(int i=0; i<msg.length(); i++){ // 시작 인덱스
             if(visited[i]) continue; // 이미 압축된 문자면 넘어가기
             
             for(int j=msg.length(); j>i; j--){ // 끝 인덱스
                 String s = msg.substring(i,j);
-                if(dictionary.contains(s)){
+                if(dictionary.containsKey(s)){
                     // 색인 번호 저장
-                    answer.add(dictionary.indexOf(s)); 
+                    answer.add(dictionary.get(s)); 
                     
                     // 압축 완료 체크
                     for(int k=i; k<j; k++) visited[k] = true;
                     
                     // 마지막 글자인 경우 제외하고 사전 추가(w+c)
-                    if(j<msg.length()-1) dictionary.add(s+msg.substring(j, j+1));
+                    if(j<msg.length()-1) dictionary.put(s+msg.substring(j, j+1), idx++);
                     break;
                 }
             }

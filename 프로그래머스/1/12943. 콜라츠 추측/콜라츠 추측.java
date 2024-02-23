b1848ef89421c141940066308cc955a1178facb7
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-02-23 13:38:20 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(long num) {
+        int cnt = 0;
+        
+        while(num>1){
+            if(cnt>500) return -1;
+            
+            if(num%2==0) num/= 2;
+            else num = num*3+1;
+            
+            cnt++;
+        }
+        
+        return cnt;
+    }
+}

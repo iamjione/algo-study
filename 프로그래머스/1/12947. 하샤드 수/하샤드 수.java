84254dc842ac250613a8e39d6cf2dee0a94a6158
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-03-03 23:37:55 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int num = x;
+        while(num>0){
+            sum += num%10;
+            num /= 10;
+        }
+        
+        if(x%sum==0) return true;
+        return false;
+    }
+}

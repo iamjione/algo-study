ca35ad336974391cea82d1eb9c4205cfd807b328
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-03-21 23:38:25 +0900

    [level 3] Title: 정수 삼각형, Time: 8.15 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -6,11 +6,11 @@ class Solution {
         
         dp[0][0] = triangle[0][0];
         for(int i=1; i<len; i++){
-            for(int j=0; j<i+1; j++){
-                if(j==0) dp[i][j] = triangle[i][j] + dp[i-1][j];
-                else if(j==i) dp[i][j] = triangle[i][j] + dp[i-1][j-1];
-                else dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j], dp[i-1][j-1]);
+            dp[i][0] = triangle[i][0] + dp[i-1][0];
+            for(int j=1; j<i; j++){
+                dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j], dp[i-1][j-1]);
             }
+            dp[i][i] = triangle[i][i] + dp[i-1][i-1];
         }
         
         for(int n : dp[len-1]){

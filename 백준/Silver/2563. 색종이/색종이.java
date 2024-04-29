f005b56aab47d138f7fb372c9b03683e538e0c57
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-04-30 05:48:06 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 13060 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -4,22 +4,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();//색종이 개수
-		int[][] arr = new int[100][100];//도화지
-
-		int cnt = 0;//겹치는 부분
 		
-		for(int i=0; i<n; i++) {
-			int r = sc.nextInt();
-			int c = sc.nextInt();
-			for(int j=0; j<10; j++) {
-				for(int k=1; k<11; k++) {
-					arr[r+j][90-c+k]+=1;
-					if(arr[r+j][90-c+k]>1) cnt++;
+		int N = sc.nextInt();
+		
+		int[][] arr = new int[101][101];
+		
+		int r,c;
+		int cnt = 0;
+		for(int i=0; i<N; i++) {
+			r = sc.nextInt();
+			c = sc.nextInt();
+			
+			for(int j=r; j<=r+9; j++) {
+				for(int k=c; k<=c+9; k++) {
+					arr[j][k]++;
+					
+					if(arr[j][k]==1) cnt++;
 				}
 			}
 		}
-		System.out.println(100*n-cnt);
+		
+		System.out.println(cnt);
 	}
 
 }

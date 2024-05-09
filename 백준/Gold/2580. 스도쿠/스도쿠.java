54435f116cb9db6ae025f32b60cc3f70195b1f48
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-05-10 01:40:12 +0900

    [Gold IV] Title: 스도쿠, Time: 404 ms, Memory: 16876 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,36 +1,32 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
 
 	static int[][] arr;
-	static StringBuilder sb = new StringBuilder();//답 저장할 스트링빌더
-	static boolean end = false;//스도쿠 완료여부 표시
+	static StringBuilder sb = new StringBuilder();
+	static boolean end = false;//스도쿠 완료 여부
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		Scanner sc = new Scanner(System.in);
+		
 		arr = new int[9][9];
 		for(int i=0; i<9; i++) {
-			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<9; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				arr[i][j] = sc.nextInt();
 			}
 		}
 		
-		sdoku(0,0);//(0,0)부터 스도쿠 시작
-		System.out.println(sb);
+		sdoku(0,0);
+		
+		System.out.println(sb.toString());
 	}
 
 	private static void sdoku(int r, int c) {
 		if(end) return;
 		
-		if(c==9) {
-			sdoku(r+1, 0);
-			return;
-		}
+		r += c/9;
+		c = c%9;
 		
 		if(r==9) {
 			for(int i=0; i<9; i++) {
@@ -39,44 +35,45 @@ public class Main {
 				}
 				sb.append('\n');
 			}
+			
 			end = true;
+			
 			return;
 		}
 		
-		if(arr[r][c]==0) {//빈칸이면 1부터 대입해보기
-			for(int n=1; n<=9; n++) {
-				//같은 행, 같은 열, 3x3칸에 n없는 경우 빈칸에 n넣기
-				if(test(r,c,n)) {
-					arr[r][c] = n;
+		if(arr[r][c]==0) {//빈칸에 1부터 대입해보기
+			for(int i=1; i<=9; i++) {
+				if(valid(r,c,i)) {
+					arr[r][c] = i;
 					sdoku(r,c+1);
+					arr[r][c] = 0;
 				}
-			}
-			//스도쿠 하다가 막혔을 때 다시 빈칸으로 만들어주고 되돌아가기!
-			arr[r][c] = 0;
-			return;
+			}			
+
+		}else { // 빈칸 아니면 다음칸으로 이동
+			sdoku(r,c+1);	
 		}
-		sdoku(r,c+1);
+		
 	}
 
-	private static boolean test(int r, int c, int n) {
-		//행 검사
+	// (r,c)에 n 넣을 수 있는지 확인하는 메서드
+	private static boolean valid(int r, int c, int n) {
+		//행, 열 검사
 		for(int i=0; i<9; i++) {
 			if(arr[r][i]==n) return false;
-		}
-		//열 검사
-		for(int i=0; i<9; i++) {
 			if(arr[i][c]==n) return false;
 		}
+
 		//3x3칸 검사
 		int x = r/3*3;//3x3칸의 시작행
 		int y = c/3*3;//3x3칸의 시작열
+		
 		for(int i=x; i<x+3; i++) {
 			for(int j=y; j<y+3; j++) {
-				if(arr[i][j]==n) {
-					return false;
-				}
+				if(arr[i][j]==n) return false;
 			}
 		}
+		
 		return true;
 	}
 

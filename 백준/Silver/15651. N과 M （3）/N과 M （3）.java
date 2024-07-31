2f8970426ab4fa0b426b672cf71f02e0d8312ad2
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-07-31 20:48:16 +0900

    [Silver III] Title: N과 M (3), Time: 584 ms, Memory: 270560 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,34 +1,35 @@
 import java.util.Scanner;
 
 public class Main {
-
-	static int N,M;
-	static int[] result;
-	static StringBuilder sb;
+	
+	static int N, M;
+	static int[] answer;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();//자연수 1부터 N까지
-		M = sc.nextInt();//M개 고르기
-
-		result = new int[M];
-		sb = new StringBuilder();
-		perm(0);
-		System.out.println(sb);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		answer = new int[M];
+		
+		recur(0);
+		
+		System.out.println(sb.toString());
 	}
 
-	private static void perm(int idx) {
-		//M개 다 골랐으면 스트링 빌더에 답 저장하고 재귀 종료
-		if(idx==M) {
-			for(int x:result) sb.append(x+" ");
-			
+	private static void recur(int idx) {
+		if(idx == M) {
+			for(int n : answer) {
+				sb.append(n+" ");
+			}
 			sb.append('\n');
 			return;
 		}
 		
-		for(int i=1; i<N+1; i++) {
-			result[idx] = i;//결과 배열에 숫자 넣어주고
-			perm(idx+1);//다음 숫자 선택하러 고고
+		for(int i=1; i<=N; i++) {
+			answer[idx] = i;
+			recur(idx+1);
 		}
 	}
 

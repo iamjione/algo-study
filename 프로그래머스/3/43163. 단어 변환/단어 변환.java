a4c53c4a65deb2288809ad58038264366affb245
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-08-02 21:40:14 +0900

    [level 3] Title: 단어 변환, Time: 0.81 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,55 +1,47 @@
-import java.util.Queue;
-import java.util.ArrayDeque;
-
 class Solution {
-    
-    int answer = 0;
 
+    int min = Integer.MAX_VALUE;
+    String[] word;
+    boolean[] visited;
+    
     public int solution(String begin, String target, String[] words) {
+        word = words;
+        visited = new boolean[words.length];
         
-        BFS(begin, target, words);
+        convert(begin, target, 0);
         
-        return answer;
+        if(min == Integer.MAX_VALUE) return 0;
+        return min;
     }
     
-    private void BFS(String begin, String target, String[] words){
-        int len = words.length;
-        int[] visited = new int[len+1]; // 방문 순서 배열
-        Queue<Integer> q = new ArrayDeque<>();
+    void convert(String curr, String target, int n){
+        // min보다 변환 횟수가 크면 리턴
+        if(n > min) return;
         
-        // begin과 한글자 차이나는 단어 큐에 넣기
-        for(int i=0; i<len; i++){
-            if(convert(begin, words[i])){
-                q.offer(i);
-                visited[i] = 1;
-            }
+        // target 만들었으면 min 갱신
+        if(curr.equals(target)){
+            min = Math.min(min, n);
+            return;
         }
         
-        // BFS 실시
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            
-            // target과 같은 단어 찾은 경우 answer에 방문 순서 저장하고 메서드 종료
-            if(words[curr].equals(target)){
-                answer = visited[curr];
-                return;
-            }
-            
-            for(int i=0; i<len; i++){
-                if(visited[i]==0 && convert(words[curr], words[i])){
-                    q.offer(i);
-                    visited[i] = visited[curr]+1;
-                }
+        // 아직 거쳐가지 않은 단어고 변환 가능하면 변환
+        for(int i=0; i<word.length; i++){
+            if(!visited[i] && convertible(curr, word[i])){
+                visited[i] = true;
+                convert(word[i], target, n+1);
+                visited[i] = false;
             }
         }
     }
     
-    // curr => next 변환 가능 여부
-    private boolean convert(String curr, String next){
-        int diff = 0;
+    // 알파벳 다른 횟수 카운트 => 2개 이상 다르면 false 반환, 1개만 다르면 true 반환
+    boolean convertible(String curr, String target){
+        int cnt = 0;
+        
         for(int i=0; i<curr.length(); i++){
-            if(curr.charAt(i) != next.charAt(i)) diff++;
-            if(diff>1) return false;
+            if(curr.charAt(i) != target.charAt(i)) cnt++;
+            
+            if(cnt>1) return false;
         }
         
         return true;

c19abdeab052340395bebae14cc73f41fd0fcf76
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-08-16 21:42:09 +0900

    [Silver II] Title: 창고 다각형, Time: 144 ms, Memory: 17148 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,54 +1,34 @@
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int[][] arr = new int[N][2];
+		int[] arr = new int[1002];
 		
 		for(int i=0; i<N; i++) {
-			arr[i][0] = sc.nextInt(); // 기둥 왼쪽 면 위치
-			arr[i][1] = sc.nextInt(); // 기둥 높이
+			arr[sc.nextInt()] = sc.nextInt();
 		}
 		
-		Arrays.sort(arr, (a, b)->a[0]-b[0]); // 기둥 왼쪽면 기준 정렬
-		
-		Stack<int[]> stack = new Stack<>();
-		stack.push(arr[0]);
-		
-		int answer = 0;
-		int idx = 1;
-		int lastIdx = 0; // 마지막으로 스택에 들어간 값의 인덱스
-		while(idx<N) {
-			if(arr[idx][1] >= stack.peek()[1]) {
-				answer += (arr[idx][0]-stack.peek()[0])*stack.peek()[1];
-				stack.pop();
-				stack.push(arr[idx]);
-				lastIdx = idx;
-			}
-			
-			idx++;
+		// 각 x값의 위치에서 1~x까지 중 최대 높이 구하기
+		int[] dp1 = new int[1002];
+		for(int i=1; i<=1000; i++) {
+			dp1[i] = Math.max(arr[i], dp1[i-1]);
 		}
 		
-		answer += arr[lastIdx][1];
-		stack.pop();
-		
-		stack.push(arr[N-1]);
-		idx = N-2;
-		while(idx>=lastIdx) {
-			if(arr[idx][1] >= stack.peek()[1]) {
-				answer += (stack.peek()[0]-arr[idx][0])*stack.peek()[1];
-				stack.pop();
-				stack.push(arr[idx]);
-			}
-			
-			idx--;
+		// 각 x값의 위치에서 x~끝까지 중 최대 높이 구하기
+		int[] dp2 = new int[1002];
+		for(int i=1000; i>=1; i--) {
+			dp2[i] = Math.max(arr[i], dp2[i+1]);
 		}
 		
-		System.out.println(answer);
+		// 최대 높이 중 최소값 더하기
+		int ans = 0;
+		for(int i=1; i<=1000; i++) {
+			ans += Math.min(dp1[i], dp2[i]);
+		}
+		
+		System.out.println(ans);
 	}
-
 }

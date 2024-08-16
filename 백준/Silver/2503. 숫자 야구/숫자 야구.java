9659424bc5818c7ee97bdd78f1d44b05c30cfce8
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-08-16 22:25:38 +0900

    [Silver III] Title: 숫자 야구, Time: 108 ms, Memory: 13316 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -2,71 +2,63 @@ import java.util.Scanner;
 
 public class Main {
 
-	static Hint[] hints;
+	static int N;
+	static int[][] arr;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); //질문 횟수
+		N = sc.nextInt(); // 질문 횟수
 		
-		hints = new Hint[N];
+		arr = new int[N][3];
 		for(int i=0; i<N; i++) {
-			hints[i] = new Hint(sc.next(), sc.nextInt(), sc.nextInt());
+			arr[i][0] = sc.nextInt(); // 숫자
+			arr[i][1] = sc.nextInt(); // 스트라이크 개수
+			arr[i][2] = sc.nextInt(); // 볼 개수
 		}
 		
 		int cnt = 0;
-		StringBuilder sb;
 		for(int i=1; i<=9; i++) {
-			sb = new StringBuilder();
-			sb.append(i);
 			for(int j=1; j<=9; j++) {
 				if(i==j) continue;
-				sb.append(j);
 				for(int k=1; k<=9; k++) {
 					if(i==k || j==k) continue;
-					sb.append(k);
-					if(isValid(sb.toString())) cnt++;
-					sb.deleteCharAt(2);
+					//ijk가 답이 될 수 있으면 카운트
+					if(valid(i,j,k)) cnt++;
 				}
-				sb.deleteCharAt(1);
 			}
 		}
+		
 		System.out.println(cnt);
+		
 	}
-	
-	private static boolean isValid(String n) {
-		for(Hint h : hints) {
-			int strike = 0;
-			int ball = 0;
+
+	private static boolean valid(int a, int b, int c) {
+		int x, y, z, strike, ball;
+		for(int i=0; i<N; i++) {
+			x = arr[i][0]/100;
+			y = (arr[i][0]%100)/10;
+			z = arr[i][0]%10;
 			
-			for(int i=0; i<3; i++) {
-				if(h.num.charAt(i) == n.charAt(i)) {
-					strike++;
-				} else {
-					for(int j=0; j<3; j++) {
-						if(h.num.charAt(j) == n.charAt(i)) {
-							ball++;
-							break;
-						}
-					}
-				}
-			}
+			// 스트라이크 수 확인하기
+			strike = 0;
 			
-			if(strike != h.s || ball != h.b) return false;
+			if(a==x) strike++;
+			if(b==y) strike++;
+			if(c==z) strike++;
+			
+			if(strike != arr[i][1]) return false;
+			
+			// 볼 수 확인하기
+			ball = 0;
+			
+			if(a==y || a==z) ball++;
+			if(b==x || b==z) ball++;
+			if(c==x || c==y) ball++;
+			
+			if(ball != arr[i][2]) return false;
 		}
 		
 		return true;
-	}
-
-	private static class Hint {
-		String num;
-		int s;
-		int b;
-		
-		private Hint(String num, int s, int b) {
-			this.num = num;
-			this.s = s;
-			this.b = b;
-		}
 	}
 
 }

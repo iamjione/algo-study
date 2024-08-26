2322d3d15a2023b79a7cd14479c5121d8081619e
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-08-26 19:36:21 +0900

    [Silver II] Title: 창고 다각형, Time: 136 ms, Memory: 17252 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -11,22 +11,20 @@ public class Main {
 			arr[sc.nextInt()] = sc.nextInt();
 		}
 		
-		// 각 x값의 위치에서 1~x까지 중 최대 높이 구하기
-		int[] dp1 = new int[1002];
+		int[] prefix_max = new int[1002];
 		for(int i=1; i<=1000; i++) {
-			dp1[i] = Math.max(arr[i], dp1[i-1]);
+			prefix_max[i] = Math.max(arr[i], prefix_max[i-1]);
 		}
 		
-		// 각 x값의 위치에서 x~끝까지 중 최대 높이 구하기
-		int[] dp2 = new int[1002];
+		int[] sufix_max = new int[1002];
 		for(int i=1000; i>=1; i--) {
-			dp2[i] = Math.max(arr[i], dp2[i+1]);
+			sufix_max[i] = Math.max(arr[i], sufix_max[i+1]);
 		}
 		
-		// 최대 높이 중 최소값 더하기
+		// prefix_max와 sufix_max 중 최소값들의 합 구하기
 		int ans = 0;
 		for(int i=1; i<=1000; i++) {
-			ans += Math.min(dp1[i], dp2[i]);
+			ans += Math.min(prefix_max[i], sufix_max[i]);
 		}
 		
 		System.out.println(ans);

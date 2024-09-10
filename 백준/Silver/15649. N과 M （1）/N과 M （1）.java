b6bb40a33687246664e3367695f9fb0201f1b58f
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-09-10 22:48:44 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 49376 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,44 +1,42 @@
 import java.util.Scanner;
 
 public class Main {
-
-	static int N,M;
+	
+	static int N, M;
 	static boolean[] visited;
-	static int[] result;
-	static StringBuilder sb;
+	static int[] ans;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();//자연수 1부터 N까지
-		M = sc.nextInt();//중복 없이 M개 고르기
+		N = sc.nextInt();
+		M = sc.nextInt();
 		
 		visited = new boolean[N+1];
-		result = new int[M];
-		sb = new StringBuilder();
-		perm(0);
-		System.out.println(sb);
+		ans = new int[M];
+		
+		recur(0);
+		
+		System.out.println(sb.toString());
 	}
 
-	private static void perm(int idx) {
-		//M개 다 골랐으면 스트링 빌더에 답 저장하고 재귀 종료
-		if(idx==M) {
-			for(int x:result) sb.append(x+" ");
-			
+	private static void recur(int idx) {
+		if(idx == M) {
+			for(int n : ans) {
+				sb.append(n+" ");
+			}
 			sb.append('\n');
 			return;
 		}
 		
-		for(int i=1; i<N+1; i++) {
-			//사용한 숫자면 컨티뉴
+		for(int i=1; i<=N; i++) {
 			if(visited[i]) continue;
 			
-			//사용하지 않은 숫자면 사용하기
-			result[idx] = i;//결과 배열에 넣어주고
-			visited[i] = true;//방문 체크
-			perm(idx+1);//다음 숫자 선택하러 고고
-			visited[i] = false;//방문 체크 원상복구
+			ans[idx] = i;
+			visited[i] = true;
+			recur(idx+1);
+			visited[i] = false;
 		}
-		
 	}
 
 }

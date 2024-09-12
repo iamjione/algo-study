0fd0725519ae5ab6327094108a24e89709595b0f
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-09-12 22:05:45 +0900

    [Silver III] Title: N과 M (8), Time: 144 ms, Memory: 25616 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -2,10 +2,11 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
+	
 	static int N, M;
-	static int[] arr, result;
-	static StringBuilder sb;
+	static int[] arr;
+	static int[] ans;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -18,30 +19,30 @@ public class Main {
 		}
 		Arrays.sort(arr);
 		
-		result = new int[M+1];
-		sb = new StringBuilder();
+		ans = new int[M];
 		
-		perm(1);
-		System.out.println(sb);
-
+		recur(0,0);
+		
+		System.out.println(sb.toString());
 	}
 
-	private static void perm(int idx) {
-		//M개 다 골랐으면 스트링 빌더에 답 저장하고 재귀 종료
-		if(idx==M+1) {
-			for(int i=1; i<=M; i++) {
-				sb.append(result[i]+" ");
+	private static void recur(int curr, int idx) {
+		if(idx==M) {
+			for(int n : ans) {
+				sb.append(n+" ");
 			}
 			sb.append('\n');
 			return;
 		}
-				
-		for(int i=0; i<N; i++) {
-			if(arr[i]>=result[idx-1]) {//이전에 선택한 숫자보다 크거나 같으면
-				result[idx] = arr[i];//결과 배열에 넣어주고
-				perm(idx+1);//다음 숫자 선택하러 고고		
-			}
-		}
+		
+		if(curr>=N) return;
+		
+		// curr번째 고르기
+		ans[idx] = arr[curr];
+		recur(curr, idx+1);
+		
+		// curr번째 안고르기
+		recur(curr+1, idx);
 	}
 
 }

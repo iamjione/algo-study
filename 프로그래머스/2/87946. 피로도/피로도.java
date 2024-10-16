bdea6b8c3d884ad1dfc8d6e2300555786a1919cd
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-10-17 03:48:38 +0900

    [level 2] Title: 피로도, Time: 5.29 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,31 +1,26 @@
 class Solution {
-    int N; // 던전 수
-    boolean[] visited; // 방문 체크 배열
-    int max = 0; // 탐험할 수 있는 최대 던전 수
-    int[][] d;
+    
+    static int answer = 0;
+    static int[][] d;
+    static boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
-        N = dungeons.length;
-        visited = new boolean[N];
         d = dungeons;
+        visited = new boolean[d.length];
         
         explore(k, 0);
         
-        return max;
+        return answer;
     }
     
-    // k: 현재 피로도, cnt: 탐험한 던전 수
-    void explore(int k, int cnt){
-        if(cnt>max) max = cnt;
-        
-        for(int i=0; i<N; i++){
-            //  이미 방문한 곳이거나 최소 필요 피로도 보다 현재 피로도가 작으면 건너뛰기
-            if(visited[i] || d[i][0]>k) continue;
+    void explore(int curr, int cnt){
+        for(int i=0; i<d.length; i++){
+            if(visited[i] || curr < d[i][0]) continue;
             
-            // 탐험할 수 있는 곳이면 탐험하기
             visited[i] = true;
-            explore(k-d[i][1], cnt+1);
-            visited[i] = false; // 원상복구
+            answer = Math.max(answer, cnt+1);
+            explore(curr-d[i][1], cnt+1);
+            visited[i] = false;
         }
     }
 }

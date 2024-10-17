56a31c5e6c285524cfa98342ecb4e05af4ca2c0a
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-10-18 03:00:33 +0900

    [Gold V] Title: LCS, Time: 128 ms, Memory: 18004 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,26 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//문자열 입력받아서 char 배열로 저장
-		char[] c1 = sc.next().toCharArray();
-		char[] c2 = sc.next().toCharArray();
-
-		int N = c1.length;//첫번째 문자열의 길이
-		int M = c2.length;//두번째 문자열의 길이
+		String a = sc.next();
+		String b = sc.next();
 		
-		int[][] dp = new int[N+1][M+1];//문자열의 길이+1 크기로 2차원 배열 만들어줌
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				//문자가 같으면 배열의 왼쪽 대각선 위의 수에 1 더한 값 저장
-				if(c1[i]==c2[j]) dp[i+1][j+1] = dp[i][j]+1;
-				//문자가 다르면 배열의 왼쪽과 위쪽 중에 더 큰 수 저장
-				else dp[i+1][j+1] = Math.max(dp[i][j+1], dp[i+1][j]);
+		int n = a.length();
+		int m = b.length();
+		
+		int[][] LCS = new int[n+1][m+1];
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				if(a.charAt(i) == b.charAt(j)) {
+					LCS[i+1][j+1] = LCS[i][j] + 1;
+				}else {
+					LCS[i+1][j+1] = Math.max(LCS[i][j+1], LCS[i+1][j]);
+				}
 			}
 		}
-		System.out.println(dp[N][M]);
+		
+		System.out.println(LCS[n][m]);
 	}
 
 }

bb34f077ffde5d7cde3be134011f00974386099f
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-10-17 16:47:28 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,7 @@
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE SKILL_CODE & (SELECT SUM(CODE)
+                   FROM SKILLCODES
+                   WHERE NAME LIKE 'Python'
+                   OR NAME LIKE 'C#') > 0
+ORDER BY ID;

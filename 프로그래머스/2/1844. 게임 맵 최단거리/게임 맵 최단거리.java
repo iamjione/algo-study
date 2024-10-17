7bd85176b8f7b2f5b4d2b87074efb11b81bcb8bb
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-10-17 21:55:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.44 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -5,49 +5,33 @@ class Solution {
         int n = maps.length;
         int m = maps[0].length;
         
-        // 이동한 칸 저장할 배열
-        int[][] cnt = new int[n][m];
+        int[][] visited = new int[n][m];
+        visited[n-1][m-1] = -1;
         
-        // BFS를 위한 큐
-        Queue<Point> q = new ArrayDeque<>();
+        int[][] d = {{1,0},{0,1},{-1,0},{0,-1}};
         
-        q.offer(new Point(0,0));// 처음 내 캐릭터의 위치 큐에 넣음
-        cnt[0][0] = 1;// 지나간 칸 카운트
+        Queue<int[]> q = new ArrayDeque<>();
         
-        // 4방 탐색 델타 값
-        int[] dr = {1,-1,0,0};
-        int[] dc = {0,0,-1,1};
+        q.add(new int[] {0,0});
+        visited[0][0] = 1;
         
-        // 큐가 빌 때까지 BFS 실시
+        int[] curr;
+        int nr, nc;
         while(!q.isEmpty()){
-            int r = q.peek().r;
-            int c = q.peek().c;
-            q.poll();
+            curr = q.poll();
             
             for(int i=0; i<4; i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-            
-                // map 벗어난 경우
+                nr = curr[0] + d[i][0];
+                nc = curr[1] + d[i][1];
+                
                 if(nr<0 || nr>=n || nc<0 || nc>=m) continue;
-                // 벽이거나 이미 방문한 길인 경우
-                if(maps[nr][nc]==0 || cnt[nr][nc] != 0) continue;
-            
-                cnt[nr][nc] = cnt[r][c] +1; // 지나간 칸 카운트
-                q.offer(new Point(nr,nc)); // 큐에 새로운 탐색 위치 넣음
+                if(visited[nr][nc] > 0 || maps[nr][nc] == 0) continue;
+                
+                q.add(new int[] {nr,nc});
+                visited[nr][nc] = visited[curr[0]][curr[1]]+1;
             }
         }
-        if(cnt[n-1][m-1] == 0) return -1;
-        else return cnt[n-1][m-1];
-    }
-    
-    private static class Point{
-        int r;
-        int c;
         
-        public Point (int r,int c){
-            this.r = r;
-            this.c = c;
-        }
+        return visited[n-1][m-1];
     }
 }

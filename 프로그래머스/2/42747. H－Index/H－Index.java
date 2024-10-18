133c93ca0f434b239bdef3468a0e82a2de34508b
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-10-18 16:23:04 +0900

    [level 2] Title: H-Index, Time: 0.90 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        
+        Arrays.sort(citations);
+        
+        for(int i=0; i<citations.length; i++){
+            if(citations[i] >= citations.length-i) {
+                return citations.length-i;
+            }
+        }
+        
+        return 0;
+    }
+}

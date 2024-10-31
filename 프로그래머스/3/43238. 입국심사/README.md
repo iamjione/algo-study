960ff165cd74e8ff4a4bbda611e938b03f4e411b
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-10-31 22:53:56 +0900

    [level 3] Title: 입국심사, Time: 207.03 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 0.41 ms
+메모리: 103 MB, 시간: 207.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 19일 23:31:17
+2024년 10월 31일 22:53:53
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,28 +2,30 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
+        long answer = 0;
+        
         Arrays.sort(times);
         
-        long answer = 0;
         long start = 1;
-        long end = (long)times[0]*n;
-       
-        while(start<=end) {
-			long mid = (start+end)/2;
-			
-			long sum = 0;
-			for(int time : times) {
-				sum += (long)mid/time;
-			}
-			
-            if(sum>=n) {
-                answer = mid;
-				end = mid-1;
-			}else {
-				start = mid+1;
+        long end = (long)times[times.length-1] * n;
+        long mid, cnt;
+        
+        while(start <= end){
+            mid = (start+end)/2;
+            
+            cnt = 0; // mid시간 동안 입국 심사 받을 수 있는 사람 수
+            for(int time : times){
+                cnt += (long)mid/time;
             }
-		}
-
+            
+            if(cnt >= n){
+                answer = mid;
+                end = mid-1;  
+            }else{
+                start = mid+1;
+            }
+        }
+        
         return answer;
     }
 }

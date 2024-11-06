ae4d2c748b4cbf2030292f567b8133b515b0f73d
지원 ᴶᴵᵂᴼᴺ <124686390+iamjione@users.noreply.github.com>
2024-11-07 01:49:40 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 13492 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,54 +1,50 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
-	static int N,M;
-	static ArrayList<Integer>[] adjlist;
-	static boolean[] visited;
 	
+	static int N;
+	static ArrayList<Integer>[] adjList;
+	static boolean[] visited;
+	static int ans = -1;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();//컴퓨터 수
-		M = sc.nextInt();//직접 연결되어 있는 컴퓨터 쌍의 수
 		
-		visited = new boolean[N+1];//방문 배열
+		N = sc.nextInt(); // 컴퓨터 수
+		int M = sc.nextInt(); // 연결된 컴퓨터 쌍의 수
 		
-		adjlist = new ArrayList[N+1];//인접 행렬 리스트
+		adjList = new ArrayList[N+1];
 		
-		//리스트 null->빈 리스트로 초기화
-		for(int i=0; i<N+1; i++) {
-			adjlist[i] = new ArrayList<>();
+		for(int i=0; i<=N; i++) {
+			adjList[i] = new ArrayList<>();
 		}
 		
-		//연결되어있는 컴퓨터의 쌍 입력받아서 인접행렬 리스트에 추가
+		int s, e;
 		for(int i=0; i<M; i++) {
-			int u = sc.nextInt();
-			int v = sc.nextInt();
+			s = sc.nextInt();
+			e = sc.nextInt();
 			
-			adjlist[u].add(v);
-			adjlist[v].add(u);
+			adjList[s].add(e);
+			adjList[e].add(s);
 		}
 		
-		dfs(1);//1을 시작 정점으로 dfs 시행
+		visited = new boolean[N+1];
 		
-		//방문 배열의 2번 인덱스부터 순회하면서 방문한 정점의 개수 카운트
-		int cnt = 0;
-		for(int i=2; i<N+1; i++) {
-			if(visited[i]==true) cnt++;
-		}
+		dfs(1);
 		
-		System.out.println(cnt);
+		System.out.println(ans);
 
 	}
-
-	private static void dfs(int i) {
-		visited[i] = true;
+	
+	static void dfs(int curr) {
+		visited[curr] = true;
+		ans++;
 		
-		for(int vertex : adjlist[i]) {
-			if(visited[vertex]==false) dfs(vertex);
+		for(int next : adjList[curr]) {
+			if(visited[next]) continue;
+			
+			dfs(next);
 		}
-		
 	}
 
 }
